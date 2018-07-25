b09b06d88738a914d91e013810b41aa38cc7a843
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2018-07-25 22:56:27 +0800

    no message

--- js-game-1.4.1/game.js
+++ js-game-1.4.1/game.js
@@ -1,6 +1,19 @@
 'use strict';
 
 
+function checkVector(obj) {
+  try {
+    if (obj instanceof Vector) {
+      return true;
+    } else {
+      throw new Error('Объект должен быть типа Vector');
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+
 class Vector {
   constructor(x=0, y=0) {
     this.x = x;
@@ -8,14 +21,8 @@ class Vector {
   }
 
   plus(obj) {
-    try {
-      if (obj instanceof Vector) {
-        return new Vector(this.x, this.y);
-      } else {
-        throw 'Можно прибавлять к вектору только вектор типа Vector';
-      }
-    } catch(err) {
-      console.log(err);
+    if ( checkVector(obj) ) {
+      return new Vector( this.x + obj.x, this.y + obj.y );
     }
   }
 
@@ -24,13 +31,53 @@ class Vector {
   }
 }
 
-
 const start = new Vector(30, 50);
 const moveTo = new Vector(5, 10);
 const finish = start.plus(moveTo.times(2));
 
 console.log(`Исходное расположение: ${start.x}:${start.y}`);
 console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
+
+
+
+class Actor {
+  constructor(pos=new Vector(0, 0), size=new Vector(1, 1), speed=new Vector(0, 0)) {
+    let isVector = false;
+    for (let arg of arguments) {
+      isVector = checkVector(arg);
+      console.log('isVector', isVector);
+    }
+  }
+}
+
+const items = new Map();
+const player = new Actor();
+items.set('Игрок', player);
+items.set('Первая монета', new Actor(new Vector(10, 10)));
+items.set('Вторая монета', new Actor(new Vector(15, 5)));
+
+function position(item) {
+  return ['left', 'top', 'right', 'bottom']
+    .map(side => `${side}: ${item[side]}`)
+    .join(', ');
+}
+
+function movePlayer(x, y) {
+  player.pos = player.pos.plus(new Vector(x, y));
+}
+
+function status(item, title) {
+  console.log(`${title}: ${position(item)}`);
+  if (player.isIntersect(item)) {
+    console.log(`Игрок подобрал ${title}`);
+  }
+}
+
+items.forEach(status);
+movePlayer(10, 10);
+items.forEach(status);
+movePlayer(5, -5);
+items.forEach(status);
 
 
 /*
